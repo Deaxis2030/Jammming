43051d77109a2ea2969d17b856a0dfc56d99d1de
Deaxis <malcolmnoel2030@gmail.com>
2024-11-18 08:02:47 -0500

    Playlist now displays added songs and allows for removal of songs

--- src/TrackInfo/Playlist.js
+++ src/TrackInfo/Playlist.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Playlist (props) {
-    const {song} = props;
+    
+    const {songList, removeSong} = props; 
+
+    const [name, setName] = useState("");
+
+    const handleChange = ({target}) => {
+        setName(target.value);
+    };
+
+    const handleRemoveClick = ({target}) => {
+        removeSong(target.value);
+    };
 
     return (
-        <li>
-           <button aria-label="Remove Song"
-                    className="removeSongBtn"
-                    onClick={handleRemoveClick}
-            >
-                &times;
-            </button>
-            <div>{song.songName} - {song.artist} - {song.album}</div>
-        </li>
+      <div className="PlayList">
+        <h1>My Playlists</h1>
+        <input aria-label="Name-of-Playlist"
+                className="playlistTitleBtn"
+                value={name}
+                type="text"
+                name="PlaylistName"
+                onChange={handleChange}
+        >
+        </input>
+            <div>
+               {songList.map((track) => (
+                <div>
+                    <h3 key={track.id}>{track.title} - {track.artist} - {track.album}</h3>
+                    <button aria-label="Remove Song"
+                            className="removeSongBtn"
+                            onClick={handleRemoveClick}
+                            value={track.id}
+                            >
+                        &times;
+                    </button>
+                </div>
+               ))}
+            </div>
+      </div>
     );
 
 };
