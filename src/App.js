98991b4cb400330d6e0af37b8a3b303e736a8602
Deaxis <malcolmnoel2030@gmail.com>
2024-11-18 09:44:01 -0500

    call SearchResults and Playlist in return function to separate them for styling. Move all related functions here and pass them down as props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 import React, {useState} from 'react';
 import SearchBar from './Search/SearchBar';
 import './App.css';
-import Tracklist from './TrackInfo/Tracklist';
 import SearchResults from './Search/SearchResults';
 import {getTracks} from './TrackInfo/Tracks'
 import SpotifyBtn from './Buttons/SpotifyBtn';
+import styles from './Stylings/App.module.css'
+import Playlist from './TrackInfo/Playlist';
+
 
 function App() {
 
-  const [songList, setSongList] = useState ([]);
+  const [text, setText] = useState("");
+    const handleTextChange = ({target}) => {
+        setText(target.value);
+    };
 
-  const addSong = (song) => {
-      setSongList ((prev) => [song, ...prev]);
+    const [results, setResults] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (text.length) {
+          setResults (getTracks(text));
+        }
+        
+    };
+
+    const [songList, setSongList] = useState ([]);
+    const addSong = (song) => {
+      console.log (song);
+      results.forEach((track) => {
+        if (song === track.id) {
+          setSongList ((prev) => {
+            if (!prev.includes(track)){
+              console.log("maybe this one?")
+             return [track, ...prev];
+            } else {
+              alert("Already added this song");
+              return [prev];
+            }
+           });
+        }
+      });   
   };
 
   const removeSong = (songToRemove) => {
-      setSongList ((songList) => {
-          songList.filter((song) => song.id !== songToRemove)
-      });
+
+    setSongList ((songList) => songList.filter((song) => song.id !== songToRemove))
+
   };
 
   return (
     <div>
-      <header>
+      <header className={styles.mainHeader}>
         Jammming
       </header>
-      <body>
+      <main>
         <div>
-          <SearchBar getTracks={getTracks}/>
+          <SearchBar text={text} handleSubmit={handleSubmit} handleTextChange={handleTextChange} getTracks={getTracks}/>
         </div>
-        
-        <div className='playList'>
-
+        <div className="results">
+          <SearchResults addSong={addSong} results={results} />
         </div>
-
-      </body>
+        <div>
+            <Playlist removeSong={removeSong} songList={songList}/>
+         </div>
+      </main>
     </div>
   );
 }
