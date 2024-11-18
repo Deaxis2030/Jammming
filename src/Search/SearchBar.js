21fee8afc6909030a67b2979cf624fdaaf911190
Deaxis <malcolmnoel2030@gmail.com>
2024-11-18 09:38:19 -0500

    move SearchResults.js from return statement and used props for submit and text change functions

--- src/Search/SearchBar.js
+++ src/Search/SearchBar.js
@@ -1,39 +1,31 @@
-import React, { useState } from "react";
-import SearchResults from "./SearchResults";
-import { getTracks } from "../TrackInfo/Tracks";
+import React from "react";
+import styles from "../Stylings/searchBarStyles.module.css"
 
-export default function SearchBar () {
+export default function SearchBar (props) {
 
-    const [text, setText] = useState("");
-    const handleTextChange = ({target}) => {
-        setText(target.value);
+    const {handleSubmit, handleTextChange, text} = props;
+
+    const onSubmitfunc = (event) => {
+      handleSubmit(event);  
+    } ;
+
+    const handleTextChangefunc = (event) => {
+        handleTextChange (event);
     };
-
-    const [results, setResults] = useState([]);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (text.length) {
-          setResults (getTracks(text));
-        }
-        
-    };
+    
 
     return (
-        <div className="searchBar">
-            <form class="example" action="action_page.php" onSubmit={handleSubmit}>
+        <div className={styles.searchBar}>
+            <form className="example" action="action_page.php" onSubmit={onSubmitfunc}>
                 <input  type="text" 
                         value={text}
                         placeholder="Search..." 
                         name="search"
-                        onChange={handleTextChange}
+                        onChange={handleTextChangefunc}
                         aria-label="search"
                 ></input>
-                <button type="submit"><i class="fa fa-search"></i></button>
+                <button type="submit"><i className="fa fa-search"></i></button>
             </form>
-            <div className="results">
-                <SearchResults results={results} />
-            </div>
         </div>
     );
 };
--- src/Search/SearchResults.js
+++ src/Search/SearchResults.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Tracklist from "../TrackInfo/Tracklist";
+import Playlist from '../TrackInfo/Playlist';
 
 
 export default function SearchResults (props) {
 
     const {results} = props;
 
+    const [songList, setSongList] = useState ([]);
+    const addSong = (song) => {
+      console.log (song);
+      results.forEach((track) => {
+        if (song === track.id) {
+          setSongList ((prev) => {
+            if (!prev.includes(track)){
+              console.log("maybe this one?")
+             return [track, ...prev];
+            } else {
+              alert("Already added this song");
+              return [prev];
+            }
+           });
+        }
+      });   
+  };
+
+  const removeSong = (songToRemove) => {
+
+    setSongList ((songList) => songList.filter((song) => song.id !== songToRemove))
+
+  };
+
     return (
         <div className='trackList'>
           {results.map((track) => (
-            <Tracklist song={track} key={track.id}/>
+            <Tracklist addSong={addSong} song={track} key={track.id}/>
           ))}
+         <div>
+            <Playlist removeSong={removeSong} songList={songList}/>
+         </div>
         </div>
     );
 
