c47fc384d72498dba5b8409907712876bc1630d4
Deaxis <malcolmnoel2030@gmail.com>
2024-11-18 09:39:58 -0500

    remove Playlist call from return statement and move add song function to App.js

--- src/Search/SearchResults.js
+++ src/Search/SearchResults.js
@@ -1,44 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 import Tracklist from "../TrackInfo/Tracklist";
-import Playlist from '../TrackInfo/Playlist';
+
 
 
 export default function SearchResults (props) {
 
-    const {results} = props;
-
-    const [songList, setSongList] = useState ([]);
-    const addSong = (song) => {
-      console.log (song);
-      results.forEach((track) => {
-        if (song === track.id) {
-          setSongList ((prev) => {
-            if (!prev.includes(track)){
-              console.log("maybe this one?")
-             return [track, ...prev];
-            } else {
-              alert("Already added this song");
-              return [prev];
-            }
-           });
-        }
-      });   
-  };
-
-  const removeSong = (songToRemove) => {
-
-    setSongList ((songList) => songList.filter((song) => song.id !== songToRemove))
-
-  };
+    const {results, addSong} = props;
 
     return (
         <div className='trackList'>
           {results.map((track) => (
             <Tracklist addSong={addSong} song={track} key={track.id}/>
           ))}
-         <div>
-            <Playlist removeSong={removeSong} songList={songList}/>
-         </div>
         </div>
     );
 
