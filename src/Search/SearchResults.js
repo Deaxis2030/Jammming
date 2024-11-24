77948199b28bdb791d7a2d667ef716e7a62469be
Deaxis <malcolmnoel2030@gmail.com>
2024-11-24 08:19:26 -0500

    now only shows a list of songs returned from searched input

--- src/Search/SearchResults.js
+++ src/Search/SearchResults.js
@@ -6,7 +6,6 @@ import Tracklist from "../TrackInfo/Tracklist";
 export default function SearchResults (props) {
 
     const {results, addSong} = props;
-
     return (
         <div className='trackList'>
           {results.map((track) => (
