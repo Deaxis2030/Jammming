b0a500395df97aa03233a58d2c8f75845ffd4bc7
Deaxis <malcolmnoel2030@gmail.com>
2024-11-24 08:23:05 -0500

    SpotifyBtn.js now properly connected to Spotify API

--- src/Buttons/SpotifyBtn.js
+++ src/Buttons/SpotifyBtn.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { postPlaylist, addSongToPlaylist } from "../TrackInfo/Tracks";
 
 export default function SpotifyBtn (props) {
-
-    const {handleSaveClick} = props;
-
+   
+    const {name, token, uriList, userId} = props;
+    
+    const handleSaveClick = async () => {
+        const playlistId = await postPlaylist (token, userId, name);
+        addSongToPlaylist(token, playlistId, uriList);
+    };
+    
     return (
         <div>
-            <button onClick={props.handleSaveClick} className="SpotifyBtn">
+            <button className="SpotifyBtn"
+                    onClick={handleSaveClick}
+            >
                 Save to Spotify
             </button>
         </div>
