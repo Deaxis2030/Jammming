b1cd23a3b4e25ee4d5c19b1fecb48b7ccb130c68
Deaxis <malcolmnoel2030@gmail.com>
2024-11-24 09:13:05 -0500

    Add a reset function to clear playlist's songlist and name after playlist is saved

--- src/Buttons/SpotifyBtn.js
+++ src/Buttons/SpotifyBtn.js
@@ -3,13 +3,14 @@ import { postPlaylist, addSongToPlaylist } from "../TrackInfo/Tracks";
 
 export default function SpotifyBtn (props) {
    
-    const {name, token, uriList, userId} = props;
+    const {name, token, uriList, userId, resetPlaylist} = props;
     
     const handleSaveClick = async () => {
         const playlistId = await postPlaylist (token, userId, name);
         addSongToPlaylist(token, playlistId, uriList);
+        resetPlaylist();
     };
-    
+
     return (
         <div>
             <button className="SpotifyBtn"
