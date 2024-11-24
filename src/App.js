c7d0fd5221218f9f27633d7b8e6f267e1f3dfa0c
Deaxis <malcolmnoel2030@gmail.com>
2024-11-24 09:17:51 -0500

    Now calls SearchBar, SearchResults, Playlist, and SpotifyBtn separately. All functions and props add with description for each function

--- src/App.js
+++ src/App.js
@@ -1,54 +1,98 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBar from './Search/SearchBar';
 import './App.css';
 import SearchResults from './Search/SearchResults';
-import {getTracks} from './TrackInfo/Tracks'
+import {getTracks, getUserId} from './TrackInfo/Tracks'
 import SpotifyBtn from './Buttons/SpotifyBtn';
 import styles from './Stylings/App.module.css'
 import Playlist from './TrackInfo/Playlist';
 
+import GetAccessToken from './Requests/GetAccessToken';
+
+
 
 function App() {
 
-  const [text, setText] = useState("");
+  const urlParams = new URLSearchParams(window.location.hash);
+  const token = urlParams.get("#access_token");
+
+  // Function below gets access token and user ID 
+  useEffect(() => {
+      const fetchToken = () => {
+        GetAccessToken();
+      };
+      if (!token) {
+        fetchToken();
+      } else {
+        setId();
+      }
+     }, []); 
+  
+    // Function below stores userID 
+    const [userId, setUserId] = useState("");
+    const setId = async () => {
+      const id = await getUserId(token); 
+      setUserId(id);
+     };
+   
+     // Function below stores value for typed input
+    const [text, setText] = useState("");
     const handleTextChange = ({target}) => {
         setText(target.value);
     };
-
+    
+    // Function below stores typed input for naming a playlist
+    const [name, setName] = useState("");
+    const handleChange = (text) => {
+        setName(text);
+    };
+    
+    // Function below stores list of songs returned searched
     const [results, setResults] = useState([]);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (text.length) {
-          setResults (getTracks(text));
-        }
-        
+    const handleSubmit = async(event) => {
+      event.preventDefault();
+      if (text.length) {
+            const list = await getTracks(text, token)
+            setResults(list);
+          }
     };
 
+    // Function below adds song to a list for creating the playlist
     const [songList, setSongList] = useState ([]);
     const addSong = (song) => {
-      console.log (song);
       results.forEach((track) => {
         if (song === track.id) {
           setSongList ((prev) => {
             if (!prev.includes(track)){
-              console.log("maybe this one?")
              return [track, ...prev];
             } else {
               alert("Already added this song");
-              return [prev];
+              return prev;
             }
            });
         }
       });   
   };
 
+  // Function below stores URI for songs in playlist
+  const [uriList, setUriList] = useState([]);
+  useEffect(() => {
+    const newUriList = songList.map(track => `spotify:track:${track.id}`);
+    setUriList(newUriList);
+  }, [songList]);
+
+  // Function below removes song from playlist
   const removeSong = (songToRemove) => {
-
     setSongList ((songList) => songList.filter((song) => song.id !== songToRemove))
-
   };
 
+  //Function below clears playlist name and songs. Used after playlist is saved.
+  const resetPlaylist = () => {
+    setSongList([]);
+    setName("");
+  }
+
+  
   return (
     <div>
       <header className={styles.mainHeader}>
@@ -56,13 +100,16 @@ function App() {
       </header>
       <main>
         <div>
-          <SearchBar text={text} handleSubmit={handleSubmit} handleTextChange={handleTextChange} getTracks={getTracks}/>
+          <SearchBar text={text} handleSubmit={handleSubmit} handleTextChange={handleTextChange}/>
         </div>
         <div className="results">
-          <SearchResults addSong={addSong} results={results} />
+          <SearchResults addSong={addSong} results={results} token={token}/>
         </div>
         <div>
-            <Playlist removeSong={removeSong} songList={songList}/>
+            <Playlist name={name} handleChange={handleChange} removeSong={removeSong} songList={songList}/>
+         </div>
+         <div>
+            <SpotifyBtn resetPlaylist={resetPlaylist} name={name} userId={userId} token={token} uriList={uriList} />
          </div>
       </main>
     </div>
