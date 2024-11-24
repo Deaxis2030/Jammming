ae9b0d86f137331fb59a28ab7455d4535823f6f0
Deaxis <malcolmnoel2030@gmail.com>
2024-11-24 08:25:08 -0500

    GetAccessToken.js created to connect to spotify and get permissions from user

---
+++ src/Requests/GetAccessToken.js
@@ -0,0 +1,28 @@
+
+
+export default function GetAccessToken () {
+    alert("login to spotify");
+    
+    const client_id = '88a024cbfe354afdbb9ccaf5dbe1ddc8';
+    const redirect_uri = 'http://localhost:3000/callback';
+
+    const generateRandomString = (length) => {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+      }
+    const state = generateRandomString(16);
+
+    localStorage.setItem("stateKey", state);
+    const scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+    
+    let url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+
+    document.location.assign(url);
+
+};
