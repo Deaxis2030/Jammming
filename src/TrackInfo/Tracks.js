48a4474a36dab1220f9d7acc5343664811afc49e
Deaxis <malcolmnoel2030@gmail.com>
2024-12-01 10:47:15 -0500

    Remove getUserID, addSongToPlaylist and PostPlaylist functions and make them into components. Tracks only exports getTracks function

--- src/TrackInfo/Tracks.js
+++ src/TrackInfo/Tracks.js
@@ -1,7 +1,6 @@
-
 let url = "https://api.spotify.com/v1/";
 
-export async function getTracks(searchInput, token) {
+export default async function getTracks(searchInput, token) {
     const response = await fetch(url + "search?q=" + searchInput + "&type=track", {
         method: "GET",
         headers: {
@@ -12,72 +11,4 @@ export async function getTracks(searchInput, token) {
     return data.tracks.items;
     
 }
-
-export async function getUserId(token) {
-  try {
-    const response = await fetch (url+"me", {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + token
-        }
-    });
-    
-    if (response.ok) {
-        const data = await response.json();
-        return data.id;
-    }
-  }
-  catch (err) {
-    console.log(`Error: ${err}`);
-  }
-}
-
-
-
-export async function postPlaylist(token, userId, name) {
-    try {
-        const response  = await fetch(`${url}users/${userId}/playlists`, {
-      
-            method: "POST",
-            headers: {
-                "Authorization": "Bearer " + token
-            },
-            body: JSON.stringify({
-            name: name,
-            public: false
-        })
-
-    });
-    if (response.ok) {
-        const data = await response.json();
-        return data.id;
-    }
-    }
-    catch (err) {
-        console.log (`Error: ${err}`);
-    }
-}
-
-
-export async function addSongToPlaylist(token, playlistId, uriList) {
-
-    const response  = await fetch(`${url}playlists/${playlistId}/tracks`, {
-      
-            method: "POST",
-            headers: {
-                "Authorization": "Bearer " + token
-            },
-            body: JSON.stringify({
-            uris: uriList,
-        })
-
-    })
-    if (response.ok) {
-        alert("Playlist saved!");
-        const data = await response.json();
-        return data;
-    }
-}
-
-
 
