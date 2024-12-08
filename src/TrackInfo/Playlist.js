7a34107c8c9b13fcec1529e6580e40b700f1add2
Deaxis <malcolmnoel2030@gmail.com>
2024-12-08 12:29:47 -0500

    add -(hyphen) to Remove-Song (aria-label on buttons)

--- src/TrackInfo/Playlist.js
+++ src/TrackInfo/Playlist.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../Stylings/Styles.module.css";
 
 export default function Playlist (props) {
     
@@ -14,29 +15,31 @@ export default function Playlist (props) {
     };
 
     return (
-      <div className="PlayList">
-        <h1>My Playlists</h1>
-        <input aria-label="Name-of-Playlist"
-                className="playlistTitleBtn"
-                value={name}
-                type="text"
-                name="PlaylistName"
-                onChange={handleTextChange}
-        >
-        </input>
-            <div>
-               {songList.map((track) => (
-                <div key={track.id}>
-                    <h3>Song Name: {track.name} - Artist(s) Name: {track.artists.map((artist, i) => (i? ", ": "") + artist.name)} - Album: {track.album.name}</h3>
-                    <button aria-label="Remove Song"
-                            className="removeSongBtn"
-                            onClick={handleRemoveClick}
-                            value={track.id}
-                            >
-                        &times;
-                    </button>
+        <div>
+            <h1>My Playlists</h1>
+             <input aria-label="Name-of-Playlist"
+                    className="playlistTitleBtn"
+                    value={name}
+                    type="text"
+                    name="PlaylistName"
+                    onChange={handleTextChange}
+                >
+                </input>
+            <div className={styles.PlaylistTracks}>
+                <div>
+                {songList.map((track) => (
+                    <div key={track.id}>
+                        <h3>Song Name: {track.name} - Artist(s) Name: {track.artists.map((artist, i) => (i? ", ": "") + artist.name)} - Album: {track.album.name}</h3>
+                        <button aria-label="Remove-Song"
+                                className={styles.removeSongBtn}
+                                onClick={handleRemoveClick}
+                                value={track.id}
+                                >
+                            &times;
+                        </button>
+                    </div>
+                ))}
                 </div>
-               ))}
             </div>
       </div>
     );
