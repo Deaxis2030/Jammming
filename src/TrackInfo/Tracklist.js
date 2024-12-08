4870c2f2373eb8390930795e32088937d0ee91ca
Deaxis <malcolmnoel2030@gmail.com>
2024-12-08 13:58:48 -0500

    add hyphen to aria-label (Add-Song)

--- src/TrackInfo/Tracklist.js
+++ src/TrackInfo/Tracklist.js
@@ -1,20 +1,20 @@
 import React from "react";
+import styles from "../Stylings/Styles.module.css";
 
 export default function Tracklist (props) {
-
-    
     const {song, addSong} = props;
+   
     const handleAddclick = ({target}) => {
         addSong(target.value);
     };
  
     const artistName = song.artists.map((artist, i) => (i? ", ": "") + artist.name);
-    console.log("artist name", artistName)
+   
     return (
-        <div className="Tracklist">
+        <div className={styles.Tracklist}>
             <div>Song: {song.name} - Artist: {artistName} - Album: {song.album.name}</div>
-            <button aria-label="Add Song"
-                    className="addSongBtn"
+            <button aria-label="Add-Song"
+                    className={styles.addSongBtn}
                     onClick={handleAddclick}
                     value={song.id}
             >
