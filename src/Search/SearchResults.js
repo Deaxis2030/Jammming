a7bbbfac140e2e31148616b29a75c82fc9fbc8d0
Deaxis <malcolmnoel2030@gmail.com>
2024-12-13 20:41:12 -0500

    No changes made to Search components

--- src/Search/SearchResults.js
+++ src/Search/SearchResults.js
@@ -5,11 +5,11 @@ import Tracklist from "../TrackInfo/Tracklist";
 
 export default function SearchResults (props) {
 
-    const {results, addSong} = props;
+    const {results, addSong, showPreview} = props;
     return (
         <div className='trackList'>
           {results.map((track) => (
-            <Tracklist addSong={addSong} song={track} key={track.id}/>
+            <Tracklist addSong={addSong} song={track} showPreview={showPreview} key={track.id}/>
           ))}
         </div>
     );
